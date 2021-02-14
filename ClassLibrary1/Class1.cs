8106631da75d11140a5488e0cb3805de5ca3579b
EdVin67 <78854982+EdVin67@users.noreply.github.com>
2021-02-14 16:42:53 +0200

    Add files via upload

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gipotenyza { 
+
+    public class Gipotez
+    {
+        public int Sum(int a, int b) 
+        {
+            return (int)Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+        }
+
+    }
+
+    public class Perimeter
+    {
+        public int c;
+        public int Per(int a, int b) 
+        {
+           c = (int)Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+            return (a + b + c);
+        }
+    }
+}
